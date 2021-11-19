dd5030e33b168710e8abcd5d97dd9f240135ae92
Persi87 <simon_black87@hotmail.co.uk>
2021-11-19 00:03:43 +0000

    commit tree 37523c081d9ecb143c4942db489e97cd29aa4581

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,72 @@
 class Owner
-  # code goes here
+
+  attr_reader :name, :species
+
+  @@all_owners = []
+   
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all_owners << self
+  end
+
+  def say_species
+    "I am a human."
+  end
+
+  def self.all
+    @@all_owners
+  end
+
+  def self.count
+    @@all_owners.count
+  end
+
+  def self.reset_all
+    @@all_owners.clear
+  end
+
+  def cats
+    Cat.all.select {|cats| cats.owner == self}
+  end
+
+  def dogs
+    Dog.all.select {|dogs| dogs.owner == self}
+  end
+
+  def buy_cat(name)
+    new_cat = Cat.new(name, self)
+  end
+
+  def buy_dog(name)
+    new_dog = Dog.new(name, self)
+  end
+
+  def walk_dogs
+    dogs.each {|dogs| dogs.mood = "happy"} # use the dogs method which already provides array of "self's" dogs, iterate through the array and change mood
+  end
+
+  def feed_cats
+    cats.each {|cats| cats.mood = "happy"} # same as above
+  end
+
+  def sell_pets
+
+    dogs.each do |dogs| 
+      dogs.mood = "nervous"
+      dogs.owner = nil
+    end
+
+    cats.each do |cats|
+     cats.mood = "nervous"
+     cats.owner = nil
+    end
+  end
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
+
+
 end
